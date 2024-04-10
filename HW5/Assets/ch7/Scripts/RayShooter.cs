a8636070fb48ae9893b40cb0bbdb838258d9ae1c
Michael Boelens <mboelens37@gmail.com>
2024-04-09 22:08:19 -0400

    HW5

---
+++ HW5/Assets/ch7/Scripts/RayShooter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerShooter : MonoBehaviour
+{
+    private Camera cam;
+    public Transform firePoint; // The point where shots will be fired from
+
+    void Start()
+    {
+        // Find the main camera in the scene
+        cam = Camera.main;
+        if (cam == null)
+        {
+            Debug.LogError("Main camera not found in the scene.");
+            return;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            // Cast a ray from the mouse cursor position
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Check if the ray hits something
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+                if (target != null)
+                {
+                    // If the hit object has a ReactiveTarget component, react to the hit
+                    target.ReactToHit();
+                    Messenger.Broadcast(GameEvent.ENEMY_HIT);
+                }
+                else
+                {
+                    // If not, create a sphere indicator at the hit point
+                    StartCoroutine(SphereIndicator(hit.point));
+                }
+            }
+        }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        // Create a sphere at the hit point
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        // Destroy the sphere after a delay
+        Destroy(sphere);
+    }
+}
---
+++ HW5/Assets/ch7/Scripts/SceneController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
+
+
+public class SceneController : MonoBehaviour {
+	[SerializeField] GameObject enemyPrefab;
+	private GameObject enemy;
+	private List <GameObject> enemies = new List<GameObject> ();
+    [SerializeField] private GameObject startGameButton;
+    [SerializeField] private GameObject exitButton;
+    [SerializeField] private GameObject winText;
+    [SerializeField] TMP_Text scoreLabel;
+    int score = 0; 
+
+    void Start() {
+        startGameButton.GetComponent<Button>().onClick.AddListener(OnStartGameButtonClick);
+        exitButton.GetComponent<Button>().onClick.AddListener(OnExitButtonClick);
+        startGameButton.SetActive (false);
+        exitButton.SetActive (false);
+        winText.SetActive (false);
+        for(int i = 0; i < 10; i++)
+		{
+			GameObject _enemy = Instantiate(enemyPrefab) as GameObject;
+			float xP = Random.Range(-10f, 10f);
+            float zP = Random.Range(-10f, 10f);
+			_enemy.transform.position = new Vector3(xP, 0, zP);
+			float angle = Random.Range(0, 360);
+			_enemy.transform.Rotate(0, angle, 0);
+			enemies.Add( _enemy );
+
+        }
+	}
+
+    void Update()
+    {
+        score = int.Parse(scoreLabel.text);
+        if (score >= 10)
+        {
+            startGameButton.SetActive(true);
+            exitButton.SetActive(true);
+            winText.SetActive(true);
+        }
+    }
+
+
+    public void OnExitButtonClick()
+    {
+        ExitGame();
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OnStartGameButtonClick()
+    {
+        Restart();
+    }
+    public void Restart()
+    { 
+        SceneManager.LoadScene("Scene");
+    }
+}
